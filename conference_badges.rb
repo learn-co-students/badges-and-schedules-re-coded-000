fb17f0ae4cdd25dcfe08ff29d183cc411fd77c73
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-10-17 17:31:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  aa = []
+  i = 0
+  name.each do |name|
+aa[i] = "Hello, my name is #{name}."
+i += 1
+end
+aa
+end
+def assign_rooms(name)
+  aa2 = []
+  i = 0
+name.each_with_index do |item , index|
+aa2[i] = "Hello, #{item}! You'll be assigned to room #{index + 1}!"
+i += 1
+end
+aa2
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |line|
+    puts "#{line}"
+  end
+  assign_rooms(array).each do |line|
+    puts "#{line}"
+end
+end
+
+# def printer(array)
+  # batch=batch_badge_creator(array)
+  # room=assign_rooms(array)
+  # counter=0
+  # array.each { |i| puts "#{batch[counter]}" puts "#{room[counter]}" counter+=1 } end
