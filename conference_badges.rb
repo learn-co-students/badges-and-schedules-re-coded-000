42fbd4ac39d3ea9062d14cb4d2a8d019655ca298
Mohammad.Daylaki <mohammad.daylaky@gmail.com>
2016-12-18 16:09:30 +0300

    Update conference_badges.rb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,31 +1,30 @@
-
-  def badge_maker (name)
-      "Hello, my name is #{name}."
-  end
-  def batch_badge_creator(array)
-    array.each do |name|
-      hello ="Hello, my name is #{name}."
-      badges =Array.new
-      badges.push(hello)
-    end
-     badges
-  end
-
-def assign_rooms(array)
-  counter =1
-  array.each do |name|
-    assigning ="Hello, #{name}! You'll be assigned to room #{counter}!"
-    counter +=1
-    room_assignments =Array.new
-    room_assignments.push(assigning)
-  end
-  room_assignments
+def batch_badge_creator(array)
+  new_array =[]
+array.each do |name|
+  x = "Hello, my name is #{name}."
+  new_array << x
 end
-def printer(attendees)
-  badges.each do |badge|
-    puts badge
-  end
-  room_assignments.each do |room|
-    puts room
-  end
+new_array
+end
+def assign_rooms(array)
+new_array =[]
+counter =1
+array.each do |name|
+  x = "Hello, #{name}! You'll be assigned to room #{counter}!"
+  counter +=1
+  new_array << x
+end
+new_array
+end
+
+def printer(array)
+  counter = 0
+  array.each do |name|
+puts badge_maker(name)
+ puts assign_rooms(array)[counter]
+ counter+=1
+end
+end
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
